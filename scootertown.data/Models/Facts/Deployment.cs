3afb952f6f46ec9a61ffcff08e056c7aa3a36254
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-03 09:49:07 -0700

    Reorder to separate reference properties.

--- scootertown.data/Models/Facts/Deployment.cs
+++ scootertown.data/Models/Facts/Deployment.cs
@@ -7,21 +7,21 @@ namespace PDX.PBOT.Scootertown.Data.Models.Facts
     public class Deployment
     {
         public long Key { get; set; }
-        public Company Company { get; set; }
-        public VehicleType VehicleType { get; set; }
+        public string AlternateKey { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
-        public string AlternateKey { get; set; }
-        public byte BatteryLevel { get; set; }
         public DbGeometry Location { get; set; }
-        public PlacementReason PlacementReason { get; set; }
+        public byte BatteryLevel { get; set; }
         public bool AllowedPlacement { get; set; }
-        public RemovalReason PickupReason { get; set; }
         public bool Reserved { get; set; }
         public bool Disabled { get; set; }
 
         public Vehicle Vehicle { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public Company Company { get; set; }
         public Calendar StartDate { get; set; }
         public Calendar EndDate { get; set; }
+        public PlacementReason PlacementReason { get; set; }
+        public RemovalReason PickupReason { get; set; }
     }
 }
