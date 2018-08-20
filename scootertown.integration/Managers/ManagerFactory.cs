1af8d90e11948b4553209cf55c88be0b3c5740bf
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-19 19:41:01 -0700

    Add factory to get managers based on configuration file

---
+++ scootertown.integration/Managers/ManagerFactory.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Extensions.Configuration;
+using PDX.PBOT.Scootertown.Integration.Managers.Implementations;
+using PDX.PBOT.Scootertown.Integration.Managers.Interfaces;
+
+namespace PDX.PBOT.Scootertown.Integration.Managers
+{
+    public static class ManagerFactory
+    {
+        public static ICompanyManager GetManager(string name, IConfigurationSection configuration)
+        {
+            switch (name)
+            {
+                case "Bird":
+                    return new BirdManager(configuration);
+                case "Lime":
+                    return new LimeManager(configuration);
+                default:
+                    throw new Exception($"No manager with name '{name}' found.");
+            }
+        }
+    }
+}
