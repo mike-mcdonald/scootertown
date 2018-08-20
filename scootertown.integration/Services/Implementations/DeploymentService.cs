d61ddb901ee5064ad655547ac3ff53b8ec47a164
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-19 19:41:32 -0700

    Add services for saving deployments and trips

---
+++ scootertown.integration/Services/Implementations/DeploymentService.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using PDX.PBOT.Scootertown.Data.Models.Dimensions;
+using PDX.PBOT.Scootertown.Data.Models.Facts;
+using PDX.PBOT.Scootertown.Data.Repositories.Interfaces;
+using PDX.PBOT.Scootertown.Integration.Infrastructure;
+using PDX.PBOT.Scootertown.Integration.Models;
+using PDX.PBOT.Scootertown.Integration.Services.Interfaces;
+
+namespace PDX.PBOT.Scootertown.Integration.Services.Implementations
+{
+    public class DeploymentService : ServiceBase<DeploymentDTO, Deployment>, IDeploymentService
+    {
+        private readonly ILogger Logger;
+        private readonly IDeploymentRepository DeploymentRepository;
+        private readonly ICompanyRepository CompanyRepository;
+        private readonly IPlacementReasonRepository PlacementReasonRepository;
+        private readonly IRemovalReasonRepository RemovalReasonRepository;
+        private readonly IVehicleRepository VehicleRepository;
+        private readonly IVehicleTypeRepository VehicleTypeRepository;
+
+        private readonly Polygon EastPortland;
+
+        public DeploymentService(
+            ILogger<DeploymentService> logger,
+            IDeploymentRepository deploymentRepository,
+            ICalendarRepository calendarRepository,
+            ICompanyRepository companyRepository,
+            IPlacementReasonRepository placementReasonRepository,
+            IRemovalReasonRepository removalReasonRepository,
+            IVehicleRepository vehicleRepository,
+            IVehicleTypeRepository vehicleTypeRepository,
+            Polygon eastPortland
+        ) : base(calendarRepository)
+        {
+            Logger = logger;
+            DeploymentRepository = deploymentRepository;
+            CompanyRepository = companyRepository;
+            PlacementReasonRepository = placementReasonRepository;
+            RemovalReasonRepository = removalReasonRepository;
+            VehicleRepository = vehicleRepository;
+            VehicleTypeRepository = vehicleTypeRepository;
+
+            EastPortland = eastPortland;
+        }
+
+        public override async Task Save(List<DeploymentDTO> items)
+        {
+            var now = DateTime.Now.ToUniversalTime();
+
+            // get all the active deployments
+            var activeDeployments = await DeploymentRepository.GetActive();
+
+            foreach (var item in items)
+            {
+                try
+                {
+                    // Use automapper for the original properties
+                    var deployment = Mapper.Map<Deployment>(item);
+
+                    // for this vehicle, is there an active deployment?
+                    var currentDeployment = activeDeployments.FirstOrDefault(x => x.VehicleKey == deployment.VehicleKey);
+                    if (currentDeployment != null)
+                    {
+                        Logger.LogTrace("Updating active deployment {key}", currentDeployment.Key);
+                        // if there is, update LastSeen
+                        currentDeployment.LastSeen = now;
+                        // only other thing that we should track as changed is a possible end time
+                        currentDeployment.EndDateKey = item.EndTime.HasValue ? (await FindOrAddCalendar(item.EndTime.Value)).Key : (int?)null;
+                        currentDeployment.EndTime = deployment.EndTime;
+                        await DeploymentRepository.Update(currentDeployment, false);
+                        // remove from active list
+                        activeDeployments.Remove(currentDeployment);
+                    }
+                    else
+                    {
+                        // if there isn't start a new one
+                        Logger.LogTrace("Creating new deployment record for vehicle {key}", deployment.VehicleKey);
+
+                        // Get the reference properties set up
+                        deployment.CompanyKey = (await FindOrAdd<Company>(CompanyRepository, item.Company, new Company { Name = item.Company })).Key;
+                        deployment.StartDateKey = (await FindOrAddCalendar(item.StartTime)).Key;
+                        deployment.EndDateKey = item.EndTime.HasValue ? (await FindOrAddCalendar(item.EndTime.Value)).Key : (int?)null;
+                        deployment.PlacementReasonKey = (await FindOrAdd<PlacementReason>(PlacementReasonRepository, item.PlacementReason, new PlacementReason { Key = item.PlacementReason })).Key;
+                        deployment.PickupReasonKey = (await FindOrAdd<RemovalReason>(RemovalReasonRepository, item.PickupReason, new RemovalReason { Key = item.PickupReason })).Key;
+                        deployment.VehicleKey = (await FindOrAdd<Vehicle>(VehicleRepository, item.Vehicle, new Vehicle { Name = item.Vehicle })).Key;
+                        deployment.VehicleTypeKey = (await FindOrAdd<VehicleType>(VehicleTypeRepository, item.VehicleType, new VehicleType { Key = item.VehicleType })).Key;
+
+                        deployment.InEastPortland = deployment.Location.Intersects(EastPortland);
+
+                        deployment.FirstSeen = deployment.LastSeen = now;
+                        await DeploymentRepository.Add(deployment, false);
+                    }
+                }
+                catch (DbUpdateException e)
+                {
+                    // this might be because we are seeing something for the first time
+                    Logger.LogWarning("Failed to save a deployment record:\n{message}\n{inner}", e.Message, e.InnerException.Message);
+                }
+            }
+
+            // for all the other active deployments that we didn't see, update the end time to now
+            var endDateKey = (await FindOrAddCalendar(now)).Key;
+            foreach (var deploymentToEnd in activeDeployments)
+            {
+                try
+                {
+                    deploymentToEnd.LastSeen = now;
+                    deploymentToEnd.EndDateKey = endDateKey;
+                    deploymentToEnd.EndTime = now.TimeOfDay;
+                    await DeploymentRepository.Update(deploymentToEnd, false);
+                }
+                catch (DbUpdateException e)
+                {
+                    // this might be because we are seeing something for the first time
+                    Logger.LogWarning("Failed to save a deployment record:\n{message}\n{inner}", e.Message, e.InnerException.Message);
+                }
+            }
+
+            await DeploymentRepository.SaveChanges();
+        }
+    }
+}
---
+++ scootertown.integration/Services/Implementations/TripService.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using PDX.PBOT.Scootertown.Data.Models.Dimensions;
+using PDX.PBOT.Scootertown.Data.Models.Facts;
+using PDX.PBOT.Scootertown.Data.Repositories.Interfaces;
+using PDX.PBOT.Scootertown.Integration.Models;
+using PDX.PBOT.Scootertown.Integration.Services.Interfaces;
+
+namespace PDX.PBOT.Scootertown.Integration.Services.Implementations
+{
+    public class TripService : ServiceBase<TripDTO, Trip>, ITripService
+    {
+        private readonly ITripRepository TripRepository;
+        private readonly ICompanyRepository CompanyRepository;
+        private readonly IPaymentTypeRepository PaymentTypeRepository;
+        private readonly IVehicleRepository VehicleRepository;
+        private readonly IVehicleTypeRepository VehicleTypeRepository;
+
+        public TripService(
+            ITripRepository tripRepository,
+            ICalendarRepository calendarRepository,
+            ICompanyRepository companyRepository,
+            IPaymentTypeRepository paymentTypeRepository,
+            IVehicleRepository vehicleRepository,
+            IVehicleTypeRepository vehicleTypeRepository
+        ) : base(calendarRepository)
+        {
+            TripRepository = tripRepository;
+            CompanyRepository = companyRepository;
+            PaymentTypeRepository = paymentTypeRepository;
+            VehicleRepository = vehicleRepository;
+            VehicleTypeRepository = vehicleTypeRepository;
+        }
+
+        public async Task<long> GetTotalTrips(string companyName) => await CompanyRepository.GetTripCount(companyName);
+
+        public override async Task Save(List<TripDTO> items)
+        {
+            var trips = new List<Trip>();
+
+            foreach (var item in items)
+            {
+                if (TripRepository.Find(item.AlternateKey) != null)
+                {
+                    // Use automapper for the original properties
+                    var trip = Mapper.Map<Trip>(item);
+
+                    // Get the reference properties set up
+                    trip.CompanyKey = (await FindOrAdd<Company>(CompanyRepository, item.Company, new Company { Name = item.Company })).Key;
+                    trip.StartDateKey = (await FindOrAddCalendar(item.StartTime)).Key;
+                    trip.EndDateKey = (await FindOrAddCalendar(item.EndTime)).Key;
+                    trip.VehicleKey = (await FindOrAdd<Vehicle>(VehicleRepository, item.Vehicle, new Vehicle { Name = item.Vehicle })).Key;
+                    trip.VehicleTypeKey = (await FindOrAdd<VehicleType>(VehicleTypeRepository, item.VehicleType, new VehicleType { Key = item.VehicleType })).Key;
+                    trip.PaymentTypeKey = (await FindOrAdd<PaymentType>(PaymentTypeRepository, item.PaymentType, new PaymentType { Key = item.PaymentType })).Key;
+                    trip.PaymentAccessKey = (await FindOrAdd<PaymentType>(PaymentTypeRepository, item.PaymentAccess, new PaymentType { Key = item.PaymentAccess })).Key;
+
+                    // add it to the database
+                    await TripRepository.Add(trip);
+                }
+            }
+        }
+    }
+}
