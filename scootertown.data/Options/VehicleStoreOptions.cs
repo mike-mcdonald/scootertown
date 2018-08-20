7480e47685b425f0836e443bcd6bd3077adfa19e
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-19 19:32:55 -0700

    Set up table structure

--- scootertown.data/Options/VehicleStoreOptions.cs
+++ scootertown.data/Options/VehicleStoreOptions.cs
@@ -4,11 +4,17 @@ namespace PDX.PBOT.Scootertown.Data.Options
     {
         public string DefaultSchema { get; set; } = null;
 
-        public TableConfiguration VehicleGroup { get; set; } = new TableConfiguration("VehicleGroup", "Dim");
-        public TableConfiguration VehicleBureau { get; set; } = new TableConfiguration("VehicleBureau", "Dim");
-        public TableConfiguration VehicleType { get; set; } = new TableConfiguration("VehicleType", "Dim");
-        public TableConfiguration VehicleLocation { get; set; } = new TableConfiguration("VehicleLocation", "Fact");
-        public TableConfiguration Vehicle { get; set; } = new TableConfiguration("Vehicle", "Dim");
         public TableConfiguration Calendar { get; set; } = new TableConfiguration("Calendar", "Dim");
+        public TableConfiguration Company { get; set; } = new TableConfiguration("Company", "Dim");
+        public TableConfiguration PaymentType { get; set; } = new TableConfiguration("PaymentType", "Dim");
+        public TableConfiguration PlacementReason { get; set; } = new TableConfiguration("PlacementReason", "Dim");
+        public TableConfiguration RemovalReason { get; set; } = new TableConfiguration("RemovalReason", "Dim");
+        public TableConfiguration Vehicle { get; set; } = new TableConfiguration("Vehicle", "Dim");
+        public TableConfiguration VehicleType { get; set; } = new TableConfiguration("VehicleType", "Dim");
+
+        public TableConfiguration Collision { get; set; } = new TableConfiguration("Collision", "Fact");
+        public TableConfiguration Complaint { get; set; } = new TableConfiguration("Complaint", "Fact");
+        public TableConfiguration Deployment { get; set; } = new TableConfiguration("Deployment", "Fact");
+        public TableConfiguration Trip { get; set; } = new TableConfiguration("Trip", "Fact");
     }
 }
