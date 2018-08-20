727af5731f39b9388f56de3e3ae959f530b68f5e
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-19 19:29:56 -0700

    Add call to check provider to aid unit testing

--- scootertown.data/Concrete/ScootertownDbContext.cs
+++ scootertown.data/Concrete/ScootertownDbContext.cs
@@ -1,7 +1,6 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
+using Microsoft.EntityFrameworkCore;
 using PDX.PBOT.Scootertown.Data.Extensions;
 using PDX.PBOT.Scootertown.Data.Models.Dimensions;
 using PDX.PBOT.Scootertown.Data.Models.Facts;
@@ -9,11 +8,11 @@ using PDX.PBOT.Scootertown.Data.Options;
 
 namespace PDX.PBOT.Scootertown.Data.Concrete
 {
-	public class ScootertownDbContext : DbContext
-	{
-		readonly VehicleStoreOptions StoreOptions;
+    public class ScootertownDbContext : DbContext
+    {
+        readonly VehicleStoreOptions StoreOptions;
         // Dimensions
-		public DbSet<Calendar> Calendar { get; set; }
+        public DbSet<Calendar> Calendar { get; set; }
         public DbSet<VehicleType> Companies { get; set; }
         public DbSet<VehicleType> PaymentTypes { get; set; }
         public DbSet<VehicleType> PlacementReasons { get; set; }
@@ -26,21 +25,30 @@ namespace PDX.PBOT.Scootertown.Data.Concrete
 
 
         public ScootertownDbContext(DbContextOptions<ScootertownDbContext> options, VehicleStoreOptions storeOptions) : base(options)
-		{
+        {
             this.StoreOptions = storeOptions ?? throw new ArgumentNullException(nameof(storeOptions));
-		}
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.ConfigureContext(StoreOptions);
+        {
+            modelBuilder.ConfigureContext(StoreOptions);
             modelBuilder.SeedData();
 
-            base.OnModelCreating(modelBuilder);
-		}
+            // Override so I can test
+            if (!Database.ProviderName.StartsWith("Npgsql"))
+            {
+                modelBuilder.Entity<Deployment>(b => b.Ignore(e => e.Location));
+                modelBuilder.Entity<Trip>(b => b.Ignore(e => e.StartPoint));
+                modelBuilder.Entity<Trip>(b => b.Ignore(e => e.EndPoint));
+                modelBuilder.Entity<Trip>(b => b.Ignore(e => e.Route));
+            }
 
-		public Task<int> SaveChangesAsync()
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public Task<int> SaveChangesAsync()
         {
             return base.SaveChangesAsync();
         }
-	}
+    }
 }
