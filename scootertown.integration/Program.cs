a524782bd8f939479c880b9e59669ae1bf641d69
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-21 21:21:53 -0700

    Cleanup and fix issue caused by moving data files up a level.

--- scootertown.integration/Program.cs
+++ scootertown.integration/Program.cs
@@ -1,25 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.FileExtensions;
-using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
-using NLog;
 using NLog.Extensions.Logging;
 using PDX.PBOT.Scootertown.Data.Concrete;
 using PDX.PBOT.Scootertown.Data.Options;
 using PDX.PBOT.Scootertown.Data.Repositories.Implementations;
 using PDX.PBOT.Scootertown.Data.Repositories.Interfaces;
-using PDX.PBOT.Scootertown.Integration.Infrastructure;
-using PDX.PBOT.Scootertown.Integration.Managers;
 using PDX.PBOT.Scootertown.Integration.Managers.Interfaces;
 using PDX.PBOT.Scootertown.Integration.Mappings;
 using PDX.PBOT.Scootertown.Integration.Services.Implementations;
@@ -29,23 +24,68 @@ namespace PDX.PBOT.Scootertown.Integration
 {
     class Program
     {
-        private static IConfiguration Configuration;
         private static List<ICompanyManager> Managers = new List<ICompanyManager>();
-        private static IServiceCollection Services;
+
         static async Task Main(string[] args)
         {
+            var pathToContentRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             var host = new HostBuilder()
+                .UseContentRoot(pathToContentRoot)
                 .ConfigureAppConfiguration((context, builder) =>
                 {
                     builder.AddJsonFile("appsettings.json");
-                    Configuration = builder.Build();
                 })
                 .ConfigureServices((context, services) =>
                 {
-                    ConfigureServices(services);
+                    // AutoMapper setup using profiles.
+                    Mapper.Initialize(cfg =>
+                    {
+                        cfg.AddProfile(typeof(DeploymentProfile));
+                        cfg.AddProfile(typeof(TripProfile));
+                    });
+
+                    var storeOptions = new VehicleStoreOptions();
+                    services.AddSingleton(storeOptions);
+
+                    services
+                        .AddEntityFrameworkNpgsql()
+                        .AddDbContext<ScootertownDbContext>(options =>
+                        {
+                            options.UseNpgsql(
+                                context.Configuration.GetConnectionString("postgres"),
+                                o => o.UseNetTopologySuite()
+                            );
+                        }, ServiceLifetime.Transient);
+
+                    // smaller dimension repositories get singletons to get better performance?
+                    services.AddSingleton<ICompanyRepository, CompanyRepository>();
+                    services.AddSingleton<IPaymentTypeRepository, PaymentTypeRepository>();
+                    services.AddSingleton<IPlacementReasonRepository, PlacementReasonRepository>();
+                    services.AddSingleton<IRemovalReasonRepository, RemovalReasonRepository>();
+                    services.AddSingleton<IVehicleTypeRepository, VehicleTypeRepository>();
+
+                    // larger dimension repositories don't to save memory
+                    services.AddTransient<ICalendarRepository, CalendarRepository>();
+                    services.AddTransient<IVehicleRepository, VehicleRepository>();
+
+                    // fact repositories 
+                    services.AddTransient<ITripRepository, TripRepository>();
+                    services.AddTransient<IDeploymentRepository, DeploymentRepository>();
+
+                    services.AddTransient<ITripService, TripService>();
+                    services.AddTransient<IDeploymentService, DeploymentService>();
+
+                    services.AddSingleton<ILoggerFactory, LoggerFactory>();
+
+                    services.AddSingleton<IHostedService, Host>();
 
                     GeoJsonReader reader = new GeoJsonReader();
-                    var area = reader.Read<Polygon>(File.ReadAllText("eastportland.json"));
+                    var area = reader.Read<Polygon>(
+                        File.ReadAllText(
+                            Path.Combine(context.HostingEnvironment.ContentRootPath, "eastportland.json")
+                        )
+                    );
                     services.AddSingleton(area);
                 })
                 .ConfigureLogging((context, logging) =>
@@ -57,47 +97,6 @@ namespace PDX.PBOT.Scootertown.Integration
                     NLog.LogManager.LoadConfiguration("nlog.config");
                 });
             await host.RunConsoleAsync();
-        }
-
-        static void ConfigureServices(IServiceCollection services)
-        {
-            // AutoMapper setup using profiles.
-            Mapper.Initialize(cfg =>
-            {
-                cfg.AddProfile(typeof(DeploymentProfile));
-                cfg.AddProfile(typeof(TripProfile));
-            });
-
-            var storeOptions = new VehicleStoreOptions();
-            services.AddSingleton(storeOptions);
-
-            services
-                .AddEntityFrameworkNpgsql()
-                .AddDbContext<ScootertownDbContext>(options =>
-                {
-                    options.UseNpgsql(
-                        Configuration.GetConnectionString("postgres"),
-                        o => o.UseNetTopologySuite()
-                    );
-                }, ServiceLifetime.Transient);
-
-            services.AddTransient<ICalendarRepository, CalendarRepository>();
-            services.AddTransient<ICompanyRepository, CompanyRepository>();
-            services.AddTransient<IPaymentTypeRepository, PaymentTypeRepository>();
-            services.AddTransient<IPlacementReasonRepository, PlacementReasonRepository>();
-            services.AddTransient<IRemovalReasonRepository, RemovalReasonRepository>();
-            services.AddTransient<IVehicleRepository, VehicleRepository>();
-            services.AddTransient<IVehicleTypeRepository, VehicleTypeRepository>();
-
-            services.AddTransient<ITripRepository, TripRepository>();
-            services.AddTransient<IDeploymentRepository, DeploymentRepository>();
-
-            services.AddTransient<ITripService, TripService>();
-            services.AddTransient<IDeploymentService, DeploymentService>();
-
-            services.AddSingleton<ILoggerFactory, LoggerFactory>();
-
-            services.AddSingleton<IHostedService, Host>();
         }
     }
 }
