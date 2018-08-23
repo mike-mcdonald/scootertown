9cc9409f68529959645be84c29a00a3130ffa668
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-23 10:25:38 -0700

    Added an options object to help read in GeoJSON files

---
+++ scootertown.integration/Options/AreasOfInterest.cs
@@ -0,0 +1,8 @@
+namespace PDX.PBOT.Scootertown.Integration.Options
+{
+    public class AreasOfInterest
+    {
+        public string NeighborhoodsFile { get; set; } = "neighborhoods.json";
+    }
+}
+
