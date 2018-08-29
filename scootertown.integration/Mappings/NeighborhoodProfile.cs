58d9b9de4b83e3d288bc83b50a91fd2271e58017
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-29 12:41:06 -0700

    Use extension class

--- scootertown.integration/Mappings/NeighborhoodProfile.cs
+++ scootertown.integration/Mappings/NeighborhoodProfile.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using NetTopologySuite.Geometries;
 using PDX.PBOT.Scootertown.Data.Models.Dimensions;
-using PDX.PBOT.Scootertown.Infrastructure.JSON;
+using PDX.PBOT.Scootertown.Infrastructure.Extensions;
 using PDX.PBOT.Scootertown.Infrastructure.Mappings;
 
 namespace PDX.PBOT.Scootertown.Integration.Mappings
@@ -13,7 +13,7 @@ namespace PDX.PBOT.Scootertown.Integration.Mappings
             CreateMap<GeoJSON.Net.Feature.Feature, Neighborhood>()
                 .ForMember(d => d.AlternateKey, opt => opt.MapFrom(s => s.Properties["OBJECTID"]))
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Properties["MAPLABEL"]))
-                .ForMember(d => d.Geometry, opt => opt.MapFrom(s => GeoJsonReaders.ReadGeoJson<Geometry>(s.Geometry)));
+                .ForMember(d => d.Geometry, opt => opt.MapFrom(s => s.Geometry.FromGeoJson<Geometry>()));
         }
     }
 }
