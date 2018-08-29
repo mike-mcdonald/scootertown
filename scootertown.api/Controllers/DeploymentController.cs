98b4b4d25c812628e9097691b2d16717536e6f59
Michael McDonald <michaelmcdonald@MacBook-Pro.local>
2018-08-29 09:36:46 -0700

    Add initial version of controllers, mappings, and DTO objects. Needto work on DTO objects to make them more reusable.

---
+++ scootertown.api/Controllers/DeploymentController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PDX.PBOT.Scootertown.API.Models;
+using PDX.PBOT.Scootertown.Data.Models.Dimensions;
+using PDX.PBOT.Scootertown.Data.Models.Facts;
+using PDX.PBOT.Scootertown.Data.Repositories.Interfaces;
+
+namespace PDX.PBOT.App.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class DeploymentController : Controller
+    {
+        private readonly ILogger Logger;
+        private readonly IDeploymentRepository DeploymentRepository;
+        private readonly ICalendarRepository CalendarRepository;
+        private readonly ICompanyRepository CompanyRepository;
+        private readonly INeighborhoodRepository NeighborhoodRepository;
+        private readonly IPlacementReasonRepository PlacementReasonRepository;
+        private readonly IRemovalReasonRepository RemovalReasonRepository;
+        private readonly IVehicleRepository VehicleRepository;
+        private readonly IVehicleTypeRepository VehicleTypeRepository;
+
+        public DeploymentController(
+            ILogger<DeploymentController> logger,
+            IDeploymentRepository deploymentRepository,
+            ICalendarRepository calendarRepository,
+            ICompanyRepository companyRepository,
+            INeighborhoodRepository neighborhoodRepository,
+            IPlacementReasonRepository placementReasonRepository,
+            IRemovalReasonRepository removalReasonRepository,
+            IVehicleRepository vehicleRepository,
+            IVehicleTypeRepository vehicleTypeRepository
+        )
+        {
+            Logger = logger;
+            DeploymentRepository = deploymentRepository;
+            CalendarRepository = calendarRepository;
+            CompanyRepository = companyRepository;
+            NeighborhoodRepository = neighborhoodRepository;
+            PlacementReasonRepository = placementReasonRepository;
+            RemovalReasonRepository = removalReasonRepository;
+            VehicleRepository = vehicleRepository;
+            VehicleTypeRepository = vehicleTypeRepository;
+        }
+
+        // GET api/deployment
+        [HttpGet]
+        public async Task<List<DeploymentDTO>> GetAsync() =>
+            (await DeploymentRepository.All()).Select(x => Mapper.Map<DeploymentDTO>(x)).ToList();
+
+        // GET api/deployment/bird/active
+        [HttpGet("{company}/active")]
+        public async Task<List<DeploymentDTO>> GetActiveAsync(string company) =>
+            (await DeploymentRepository.GetActive(company)).Select(x => Mapper.Map<DeploymentDTO>(x)).ToList();
+
+        // GET api/deployment/5
+        [HttpGet("{key}")]
+        public async Task<IActionResult> GetAsync(long key)
+        {
+            var deployment = await DeploymentRepository.Find(key);
+            return Ok(Mapper.Map<DeploymentDTO>(deployment));
+        }
+
+        // POST api/deployment
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]DeploymentDTO value)
+        {
+            var now = DateTime.Now;
+
+            var deployment = Mapper.Map<Deployment>(value);
+
+            var vehicle = await VehicleRepository.Find(value.Vehicle) ?? await VehicleRepository.Add(new Vehicle { Name = value.Vehicle });
+            var endDateTask = value.EndTime.HasValue ? CalendarRepository.Find(value.EndTime.Value) : Task.FromResult<Calendar>(null);
+
+            // need the vehicle so we can test for active deployments
+            deployment.VehicleKey = vehicle.Key;
+
+            var activeDeployments = await DeploymentRepository.GetActive();
+            // for this vehicle, is there an active deployment?
+            var currentDeployment = activeDeployments.FirstOrDefault(x => x.VehicleKey == deployment.VehicleKey);
+            if (currentDeployment != null)
+            {
+                // if there is, update LastSeen
+                currentDeployment.LastSeen = now;
+                // only other thing that we should track as changed is a possible end time
+                currentDeployment.EndDateKey = (await endDateTask)?.Key;
+                currentDeployment.EndTime = deployment.EndTime;
+                await DeploymentRepository.Update(currentDeployment, false);
+            }
+            else
+            {
+                // if there isn't start a new one
+
+                // Get the reference properties set up
+
+                // write this first so we don't start two operations
+                deployment.EndDateKey = (await endDateTask)?.Key;
+
+                var companyTask = CompanyRepository.Find(value.Company);
+                var startDateTask = CalendarRepository.Find(value.StartTime);
+
+                deployment.CompanyKey = (await companyTask).Key;
+                deployment.StartDateKey = (await startDateTask).Key;
+
+                deployment.FirstSeen = deployment.LastSeen = now;
+                await DeploymentRepository.Add(deployment, false);
+            }
+
+            try
+            {
+                await DeploymentRepository.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error adding deployment:\n{message}\n{inner}", e.Message, e.InnerException.Message);
+                return BadRequest(e.ToString());
+            }
+            return Ok(Mapper.Map<DeploymentDTO>(deployment));
+        }
+
+        // PUT api/deployment/5
+        [HttpPut("{key}")]
+        public void Update(long key, [FromBody]DeploymentDTO value)
+        {
+        }
+
+        // POST api/deployment/5/end
+        [HttpPost("{key}/end")]
+        public async Task<IActionResult> EndDeployment(long key)
+        {
+            Logger.LogInformation($"Ending deployment {key}...");
+            try
+            {
+                var deployment = await DeploymentRepository.Find(key);
+                var now = DateTime.Now;
+                var calendarTask = CalendarRepository.Find(now);
+                deployment.EndTime = now.TimeOfDay;
+                deployment.LastSeen = now;
+                deployment.EndDateKey = (await calendarTask).Key;
+
+                await DeploymentRepository.Update(deployment);
+
+                return Ok(Mapper.Map<DeploymentDTO>(deployment));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+        }
+    }
+}
---
+++ scootertown.api/Controllers/TripController.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PDX.PBOT.Scootertown.API.Models;
+using PDX.PBOT.Scootertown.Data.Models.Dimensions;
+using PDX.PBOT.Scootertown.Data.Models.Facts;
+using PDX.PBOT.Scootertown.Data.Repositories.Interfaces;
+
+namespace PDX.PBOT.App.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TripController : Controller
+    {
+        private readonly ILogger Logger;
+        private readonly ITripRepository TripRepository;
+        private readonly ICalendarRepository CalendarRepository;
+        private readonly ICompanyRepository CompanyRepository;
+        private readonly INeighborhoodRepository NeighborhoodRepository;
+        private readonly IPaymentTypeRepository PaymentTypeRepository;
+        private readonly IVehicleRepository VehicleRepository;
+        private readonly IVehicleTypeRepository VehicleTypeRepository;
+
+        public TripController(
+            ILogger<TripController> logger,
+            ITripRepository tripRepository,
+            ICalendarRepository calendarRepository,
+            ICompanyRepository companyRepository,
+            INeighborhoodRepository neighborhoodRepository,
+            IPaymentTypeRepository paymentTypeRepository,
+            IVehicleRepository vehicleRepository,
+            IVehicleTypeRepository vehicleTypeRepository
+        )
+        {
+            Logger = logger;
+            TripRepository = tripRepository;
+            CalendarRepository = calendarRepository;
+            CompanyRepository = companyRepository;
+            NeighborhoodRepository = neighborhoodRepository;
+            PaymentTypeRepository = paymentTypeRepository;
+            VehicleRepository = vehicleRepository;
+            VehicleTypeRepository = vehicleTypeRepository;
+        }
+
+        // GET api/trip
+        [HttpGet]
+        public async Task<long> GetAsync() =>
+            await TripRepository.Count();
+
+
+        // GET api/trip/5
+        [HttpGet("{key}")]
+        public async Task<IActionResult> GetAsync(long key)
+        {
+            var deployment = await TripRepository.Find(key);
+            return Ok(Mapper.Map<TripDTO>(deployment));
+        }
+
+        // GET api/trip/lime
+        [HttpGet("{company}")]
+        public async Task<long> GetAsync(string company) =>
+            await TripRepository.CountByCompany(company);
+
+        // POST api/trip
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]TripDTO value)
+        {
+            var now = DateTime.Now;
+
+            var existingTask = TripRepository.Find(value.AlternateKey);
+
+            var trip = Mapper.Map<Trip>(value);
+
+            var vehicle = await VehicleRepository.Find(value.Vehicle) ?? await VehicleRepository.Add(new Vehicle { Name = value.Vehicle });
+            var endDateTask = CalendarRepository.Find(value.EndTime);
+
+            trip.VehicleKey = vehicle.Key;
+
+            // if there isn't start a new one
+
+            // await this first before finding the next date
+            trip.EndDateKey = (await endDateTask).Key;
+
+            // Get the reference properties set up
+            var companyTask = CompanyRepository.Find(value.Company);
+            var startDateTask = CalendarRepository.Find(value.StartTime);
+            var vehicleTypeTask = VehicleTypeRepository.Find(value.VehicleType);
+            var paymentTypeTask = PaymentTypeRepository.Find(value.PaymentType);
+            var paymentAccessTask = PaymentTypeRepository.Find(value.PaymentAccess);
+
+            trip.CompanyKey = (await companyTask).Key;
+            trip.StartDateKey = (await startDateTask).Key;
+            trip.VehicleTypeKey = (await vehicleTypeTask).Key;
+            trip.PaymentTypeKey = (await paymentTypeTask).Key;
+            trip.PaymentAccessKey = (await paymentAccessTask).Key;
+
+            var existing = await existingTask;
+
+            if (existing != null)
+            {
+                trip.Key = existing.Key;
+            }
+
+            var upsertTask = existing == null ? TripRepository.Add(trip, false) : TripRepository.Update(trip, false);
+
+            try
+            {
+                await upsertTask;
+                await TripRepository.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error adding deployment:\n{message}\n{inner}", e.Message, e.InnerException.Message);
+                return BadRequest(e.ToString());
+            }
+            return Ok(Mapper.Map<TripDTO>(trip));
+        }
+
+        // PUT api/trip/5
+        [HttpPut("{key}")]
+        public void Update(long key, [FromBody]TripDTO value)
+        {
+        }
+    }
+}
