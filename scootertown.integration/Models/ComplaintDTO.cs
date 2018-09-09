225afcc319c2d54706f3e6bb974f25c517be1459
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2018-09-08 23:11:47 -0700

    Add complaint classes to integration

--- scootertown.integration/Models/ComplaintDTO.cs
+++ scootertown.integration/Models/ComplaintDTO.cs
@@ -1,7 +1,25 @@
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+
 namespace PDX.PBOT.Scootertown.Integration.Models
 {
     public class ComplaintDTO
     {
-        public int Key { get; set; }
+        [JsonProperty(PropertyName = "company_name")]
+        public string CompanyName { get; set; }
+        [JsonProperty(PropertyName = "device_type")]
+        public byte? VehicleTypeKey { get; set; }
+        [JsonProperty(PropertyName = "device_id")]
+        public string VehicleName { get; set; }
+        [JsonProperty(PropertyName = "date_submitted")]
+        public long? SubmittedDate { get; set; }
+        [JsonProperty(PropertyName = "location")]
+        public Point Location { get; set; }
+        [JsonProperty(PropertyName = "complaint_details")]
+        public string ComplaintDetails { get; set; }
+        [JsonProperty(PropertyName = "complaints")]
+        public string[] Complaints { get; set; }
+        [JsonProperty(PropertyName = "complaint_type")]
+        public int? ComplaintTypeKey { get; set; }
     }
 }
---
+++ scootertown.integration/Services/Interfaces/IComplaintService.cs
@@ -0,0 +1,6 @@
+using PDX.PBOT.Scootertown.Integration.Models;
+
+namespace PDX.PBOT.Scootertown.Integration.Services.Interfaces
+{
+    public interface IComplaintService : IService<ComplaintDTO> { }
+}
