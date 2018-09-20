cc8a70cae504c3a5480ad695c72a4f4feb04cd3e
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2018-09-20 09:45:35 -0700

    Add mapping to break out x/y for trips

--- scootertown.integration/Mappings/TripProfile.cs
+++ scootertown.integration/Mappings/TripProfile.cs
@@ -12,7 +12,11 @@ namespace PDX.PBOT.Scootertown.Integration.Mappings
             CreateMap<Models.TripDTO, API.Models.TripDTO>()
                 .ForMember(d => d.Key, opt => opt.Ignore())
                 .ForMember(d => d.StartTime, opt => opt.MapFrom(s => s.StartTime.ToDateTime()))
-                .ForMember(d => d.EndTime, opt => opt.MapFrom(s => s.EndTime.ToDateTime()));
+                .ForMember(d => d.EndTime, opt => opt.MapFrom(s => s.EndTime.ToDateTime()))
+                .ForMember(d => d.StartX, opt => opt.MapFrom(s => s.StartPoint.Coordinates.Longitude))
+                .ForMember(d => d.StartY, opt => opt.MapFrom(s => s.StartPoint.Coordinates.Latitude))
+                .ForMember(d => d.EndX, opt => opt.MapFrom(s => s.EndPoint.Coordinates.Longitude))
+                .ForMember(d => d.EndY, opt => opt.MapFrom(s => s.EndPoint.Coordinates.Latitude));
         }
     }
 }
