06bcfccb6197ab40e9cbc1ae07dc0732e3deb75e
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2018-09-24 17:14:50 -0700

    Add ModelBase inheiritance so IRepository for class is available. Don't map it though, since it isn't a valid property.

--- scootertown.data/Models/Bridges/StreetSegmentGroup.cs
+++ scootertown.data/Models/Bridges/StreetSegmentGroup.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using PDX.PBOT.Scootertown.Data.Models.Dimensions;
 
 namespace PDX.PBOT.Scootertown.Data.Models.Bridges
 {
-    public class StreetSegmentGroup
+    public class StreetSegmentGroup : ModelBase
     {
-        public int? StreetSegmentGroupKey { get; set; }
+        [NotMapped]
+        public new int Key { get; set; }
+        public long? StreetSegmentGroupKey { get; set; }
         public Dimensions.StreetSegmentGroup SegmentGroup { get; set; }
         public int? StreetSegmentKey { get; set; }
         public StreetSegment StreetSegment { get; set; }
