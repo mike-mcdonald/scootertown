4cbdbcf15194fcfd2e0e4e7d62505c65a1c8b65e
Michael McDonald <michael.mcdonald@portlandoregon.gov>
2018-11-28 12:22:59 -0800

    Add mapping for new attributes

--- scootertown.integration/Mappings/BicyclePathProfile.cs
+++ scootertown.integration/Mappings/BicyclePathProfile.cs
@@ -12,10 +12,11 @@ namespace PDX.PBOT.Scootertown.Integration.Mappings
         public BicyclePathProfile()
         {
             CreateMap<GeoJSON.Net.Feature.Feature, BicyclePath>()
-                .ForMember(d => d.AlternateKey, opt => opt.MapFrom(s => s.Properties["objectId"]))
+                .ForMember(d => d.AlternateKey, opt => opt.MapFrom(s => s.Properties["tranPlanID"]))
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Properties["name"]))
                 .ForMember(d => d.Buffer, opt => opt.UseValue(BUFFER_DISTANCE))
                 .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Properties["status"]))
+                .ForMember(d => d.Type, opt => opt.MapFrom(s => s.Properties["facility"]))
                 .ForMember(d => d.Geometry, opt => opt.MapFrom(s => s.Geometry.FromGeoJson<Geometry>()))
                 .ForMember(d => d.X, opt => opt.MapFrom(s => s.Geometry.FromGeoJson<Geometry>().Centroid.Coordinates[0].X))
                 .ForMember(d => d.Y, opt => opt.MapFrom(s => s.Geometry.FromGeoJson<Geometry>().Centroid.Coordinates[0].Y));
